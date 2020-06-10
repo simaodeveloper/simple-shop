07b8491f37ade17efc56e5f9ec16943a3e05bffb
Daniel Simão <simaodeveloper@gmail.com>
2020-06-09 21:59:38 -0300

    chore: working on error handler

--- src/assets/scripts/services/api.js
+++ src/assets/scripts/services/api.js
@@ -1,9 +1,11 @@
 const API = (({ API_URL }) => {
-  const request = url => fetch(url).then(response => response.json())
+  const request = (url) => fetch(url)
+  .then((response) => response.json())
+  .catch((err) => //... errorhandler);
 
   return {
-    async fetchProducts () {
-      return await request(`${API_URL}/products.json`)
-    }
-  }
-})(config)
+    async fetchProducts() {
+      return await request(`${API_URL}/products.json`);
+    },
+  };
+})(config);
